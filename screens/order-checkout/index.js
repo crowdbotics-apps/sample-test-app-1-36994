a5d03ac6dff431345f77aa33533cc79fc7e75a77
Adnan Khan <adnan.khan@crowdbotics.com>
2023-10-19 12:56:00 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/order-checkout/index.js
+++ screens/order-checkout/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 
-const OrderCheckoutScreen = (params) => {
-  return (
-    <View style={styles.container}>
+const OrderCheckoutScreen = params => {
+  return <View style={styles.container}>
       <Text style={styles.headingTxt}>Order Checkout Screen</Text>
-    </View>
-  );
+    </View>;
 };
 
 const styles = StyleSheet.create({
@@ -22,5 +20,4 @@ const styles = StyleSheet.create({
     marginVertical: 12
   }
 });
-
 export default OrderCheckoutScreen;
